35441c21ef36d21657d10c8468ca04b81316332a
Luz Lara <luzalexandra.lara@gmail.com>
2026-03-23 21:20:50 -0400

    successful login navigate to duck

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,7 @@ import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
 
+const [userData, setUserData] = useState({username: "", email: ""})
 const [isLoggedIn, setIsLoggedIn] = useState(false);
 
  const navigate = useNavigate();
@@ -28,6 +29,22 @@ const handleRegistration = ({
     })
     .catch(console.error);
   }
+}
+const handleLogin =({username, password}) =>{
+  if (!username || !password){
+    return;
+  }
+  auth
+  .authorize(username, password)
+  .then((data)=>{
+    if(data.jwt){
+      setUserData(data.user);
+      setIsLoggedIn(true);
+      navigate("/ducks");
+    }
+  }    
+)
+  .catch(console.error)
 }
 
   return (
@@ -48,7 +65,7 @@ const handleRegistration = ({
         path="/login"
         element={
           <div className="loginContainer">
-            <Login />
+            <Login  handleLogin={handleLogin} />
           </div>
         }
       />
